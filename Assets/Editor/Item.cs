b6fb4540a59df21d1cfd530c2127d96c407f7b75
alelievr <alelievr@student.42.fr>
2017-09-12 19:50:31 +0200

    added graph files

---
+++ Assets/Editor/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+// A behaviour that is attached to a playable
+public class Item : ScriptableObject
+{
+	public List< string > strs = new List< string >();
+
+}
---
+++ Assets/Editor/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu]
+public class Weapon : Item {
+
+	public int		damage;
+
+	void OnEnable()
+	{
+		Debug.Log("Enable Weapon");
+	}
+
+	void OnDisable()
+	{
+		Debug.Log("Disable Weapon");
+	}
+}
